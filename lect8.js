c3076a1590d9b26c0f7b7727a65a13e938de4d91
Rabten <rabten_66@yahoo.com>
2017-02-16 14:44:51 +0600

    modified.

--- lect8.js
+++ lect8.js
@@ -4,11 +4,16 @@
   angular.module("DIApp",[]);
   .controller("DIController",DIController);
 
-  function DIController($scope,$filter){
+  function DIController($scope,$filter,$injector){
     $scope.name="SChoki";
     $scope.upper = function(){
       var upCase = $filter("uppercase");
       $scope.name = upCase($scope.name);
     };
+    console.log($injector.Annonate(DIController));
+  }
+  function annonateMe(name,job,blah){
+    return "Blah";
+    console.log(DIController.toString());
   }
 })();
