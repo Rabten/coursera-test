188ad403700afad9472084c54a579e6f16b1a6a1
Rabten <rabten_66@yahoo.com>
2017-02-16 10:31:07 +0600

    lecture 7

--- lect.js
+++ lect.js
@@ -4,16 +4,16 @@
   angular.module('NameCalculator',[])
   .controller("NameCalculatorController",function($scope){
     $scope.name = "";
-    $scope.totalValue = 0;
-    $scope.dispalyNumeric = function(){
+    $scope.totalValue = 0;//to get the total number of the strings
+    $scope.displayNumeric = function(){//function to recieve the total sting value
       var totalNameValue = calculatorNumericForString($scope.name);
       $scope.totalValue = totalNameValue;
 
     };
     function calculatorNumericForString(string){
       var totalStringvalue = 0;
-      for (var i=0; i<string.length;i++){
-        totalStringvalue+=string.charCodeAt(i);
+      for (var i=0; i<string.length;i++){//string.length iterate till the lenght of the String.
+        totalStringvalue+=string.charCodeAt(i);//charCodeAtis the system function
       }
       return totalStringvalue;
 
